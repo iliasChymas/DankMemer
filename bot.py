c46db1af3ab3263ffd53a0c6b1db07059232f6b4
SupremeKim <45954373+iliasChymas@users.noreply.github.com>
2022-08-04 02:09:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,23 +22,23 @@ async def on_message(message):
         embed.set_image(url=file)
         
         await message.channel.send(embed=embed)
-    elif "!porn" in message.content.lower():
-        m = message.content.lower().split(' ')
-        print(m)
-        print(message.author)
-        if len(m) == 1: m.append('porn')
-        print(len(m))
-        if m[1] not in ['blonde', 'redhead', 'boobs', 'glasses', 'ass', "milf", "japanese", 'help', 'porn']:
-            await message.channel.send(content="Wrong m type '!porn help' to get all ms", file=discord.File('./rick_roll.gif') )
-        elif m[1] != "help":
+#     elif "!command" in message.content.lower():
+#         m = message.content.lower().split(' ')
+#         print(m)
+#         print(message.author)
+#         if len(m) == 1: m.append('porn')
+#         print(len(m))
+#         if m[1] not in ['command_option_0', 'command_option_1', 'command_option_2', 'command_option_3', 'command_option_4', "command_option_5", "command_option_6", 'command_option_7', 'command_option_8']:
+#             await message.channel.send(content="Wrong m type '!command help' to get all ms", file=discord.File('./rick_roll.gif') )
+#         elif m[1] != "help":
             
-            url, title = get_hot(m[1])
-            embed = discord.Embed(title=title)
-            embed.set_image(url=url)
-            print("got porn")
-            await message.channel.send(embed=embed)
-        else:
-            await message.channel.send(content="COMMANTS FOR PORN: \n blonde, redhead, boobs, glasses, ass, milf, japanese,")
+#             url, title = get_hot(m[1])
+#             embed = discord.Embed(title=title)
+#             embed.set_image(url=url)
+#             print("got porn")
+#             await message.channel.send(embed=embed)
+#         else:
+#             await message.channel.send(content="COMMANTS FOR PORN: \n blonde, redhead, boobs, glasses, ass, milf, japanese,")
     elif "!pun" == message.content.lower():
         title, text = get_pun()
         await message.channel.send(content=title + '\n' + "||" + text + "||")
